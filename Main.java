3c02005322edf0b45b6eb02589e1ae0ad4f6e8c7
Rachelle Anne Reyes <58576047+rachelleannereyesss@users.noreply.github.com>
2019-12-06 09:22:00 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
+package main;
+
+
+import java.util.*;
 public class Main {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    
+    public static void main(String[] args {
+        Scanner scn = new Scanner(System.in);
+        String name= "Rachelle Anne Reyes";
+        System.out.println("Hello World ");
+        System.out.println("What's your name? ");
+        name = scn.nextLine();
+        System.out.println("How old are you? ");
+        int age=scn.nextInt();
+        System.out.println("What is your address? ");
+        String address = scn.nextLine();
+        address = scn.nextLine();
+        System.out.println("Hi " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("Your adress is " + address);
+        
+        
+        
+        
+    }
 }
